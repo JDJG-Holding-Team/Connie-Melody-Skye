d343f8acc78ed12267776d187e1bc792ac15796b
JDJG <ghostzap123@gmail.com>
2024-02-19 22:20:43 -0500

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "music" (
+	"user_id"	bigint,
+	"url"	TEXT,
+	"Service"	TEXT
+)
+
+CREATE TABLE "to_watch" (
+	"user_id"	bigint,
+	"url"	TEXT,
+	"Service"	TEXT
+)
+
+CREATE TABLE "watched_videos" (
+	"user_id"	bigint,
+	"url"	TEXT,
+	"Service"	TEXT
+)
