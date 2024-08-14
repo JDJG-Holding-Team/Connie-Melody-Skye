feacd4562733d2385e7d0c1ecedcfcf95b932c51
JDJG <ghostzap123@gmail.com>
2024-08-14 02:36:47 -0400

    Removes unused tables.

--- table.sql
+++ table.sql
@@ -1,37 +1,3 @@
-CREATE TABLE IF NOT EXISTS music (
-	user_id	BIGINT,
-	url	TEXT,
-	service	TEXT
-);
-CREATE TABLE IF NOT EXISTS watched_videos (
-	user_id	BIGINT,
-	url	TEXT,
-	service	TEXT
-);
-CREATE TABLE IF NOT EXISTS to_watch (
-	user_id	BIGINT,
-	url	TEXT,
-	service	TEXT
-);
-
-CREATE TABLE IF NOT EXISTS MISC_VIDEOS (
-	user_id	BIGINT,
-	url	TEXT,
-	service	TEXT
-);
-
-CREATE TABLE IF NOT EXISTS tech_videos (
-	user_id	BIGINT,
-	url	TEXT,
-	service	TEXT
-);
-
-CREATE TABLE IF NOT EXISTS anime_videos (
-	user_id	BIGINT,
-	url	TEXT,
-	service	TEXT
-);
-
 CREATE TABLE CONTENT(
   user_id BIGINT NOT NULL,
   url TEXT NOT NULL,
