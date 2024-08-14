7dc5de2503c95bf267c1aee90ca038cd09175eaa
JDJG <ghostzap123@gmail.com>
2024-08-13 21:10:58 -0400

    There fixed.

--- utils/extra.py
+++ utils/extra.py
@@ -1,9 +1,9 @@
 import enum
 
 class ContentType(enum.IntEnum):
-    music : 0
-    tech: 1
-    anime : 2
-    misc: 3
-    watch: 4
-    watched: 5
+    music = 0
+    tech = 1
+    anime = 2
+    misc = 3
+    watch = 4
+    watched = 5
